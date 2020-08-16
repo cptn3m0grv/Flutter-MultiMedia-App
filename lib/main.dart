82fc39c469831404f10ceabd54d632f6c8faf3f0
Gaurav Goyal <cptn3m0grv@gmail.com>
2020-08-16 17:06:05 +0530

    source update

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,13 @@ class MyApp extends StatelessWidget{
       routes: <String, WidgetBuilder>{
         '/' : (context) => HomePage(),
         '/offlineSong' : (context) => OfflineSong(),
-        // '/onlineSong' : (context) => OnlineSong(),
+        '/onlineSong' : (context) => OnlineSong(),
       },
       debugShowCheckedModeBanner: false,
     );
   }
 }
+
 class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
@@ -50,7 +51,6 @@ class HomePage extends StatelessWidget{
                   Card(
                     child: FlatButton(
                       onPressed: () {
-                        print("First button");
                         Navigator.pushNamed(context, '/offlineSong');
                       },
                       child: Text(
@@ -80,7 +80,7 @@ class HomePage extends StatelessWidget{
                   Card(
                     child: FlatButton(
                       onPressed: () {
-                        print("Third button");
+                        Navigator.pushNamed(context, '/onlineSong');
                       },
                       child: Text(
                         "            Online Song            ",
@@ -127,7 +127,6 @@ class OfflineSong extends StatelessWidget{
       audio.play("audios/song.mp3");
       playing = 1;
       stopped = 0;
-      print("Audio Stopped");
     }
   }
 
@@ -135,7 +134,6 @@ class OfflineSong extends StatelessWidget{
     if(playing == 1){
       player.pause();
       playing = 0;
-      print("Audio Paused");
     }
   }
 
@@ -144,7 +142,6 @@ class OfflineSong extends StatelessWidget{
       player.stop();
       playing = 0;
       stopped = 1;
-      print("Audio Stopped");
     }
   }
 
@@ -167,14 +164,14 @@ class OfflineSong extends StatelessWidget{
         height: MediaQuery.of(context).size.height,
         child: Column(
           children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(30),
-              width: 200,
-              height: 200,
-              color: Colors.red,
+            Image.asset(
+                'assets/images/offline_song_image.jpg',
+                fit: BoxFit.cover,
+                height: 300,
             ),
             Container(
-              width: 170,
+              margin: EdgeInsets.all(10),
+              width: 180,
               child: Card(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(50),
@@ -201,6 +198,109 @@ class OfflineSong extends StatelessWidget{
                       icon: Icon(Icons.pause), 
                       onPressed: () {
                         pauseButton();
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class OnlineSong extends StatelessWidget{
+  
+  String url;
+  var stopped = 1;
+  var playing = 0;
+
+  playButton() async{
+    if(playing == 0 || stopped == 1){
+      player.play(url);
+      playing = 1;
+      stopped = 0;
+    }
+  }
+
+  pauseButton(){
+    if(playing == 1){
+      player.pause();
+      playing = 0;
+    }
+  }
+
+  stopButton(){
+    if(playing == 1 && stopped == 0){
+      player.stop();
+      playing = 0;
+      stopped = 1;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Online Mode",
+          style: TextStyle(
+            fontFamily: 'Satisfy',
+            fontSize: 30,
+          ),
+        )
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onChanged: (value) {
+                url = value;
+              },
+              decoration: InputDecoration(
+                hintText: 'URL of the song',
+                prefixIcon: Icon(Icons.search),
+              ),
+            ),
+            Image.asset(
+              'assets/images/offline_song_image.jpg',
+               fit: BoxFit.cover,
+               height: 300,
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              width: 180,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                color: Color.fromARGB(255, 193, 200, 228),
+                child: Row(
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.stop), 
+                      onPressed: () {
+                        // stopButton();
+                      },
+                    ),
+                    // Text("      "),
+                    IconButton(
+                      iconSize: 40,
+                      icon: Icon(Icons.play_arrow), 
+                      onPressed: () {
+                        // playButton();
+                      },
+                    ),
+                    // Text("     "),
+                    IconButton(
+                      icon: Icon(Icons.pause), 
+                      onPressed: () {
+                        // pauseButton();
                       },
                     ),
                   ],
